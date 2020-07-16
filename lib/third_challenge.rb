b8d30cc67f6c7014d94db0eba3f7c8ca735ab09e
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-16 06:34:42 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,7 @@ def third_challenge
       :matriarch => {:name => "Lady Montague", :age => "54"
         #your key/value pairs here
         },
-      :hero => {:hero => "Romeo", :age => "15", :status => "alive"
+      :hero => {:name => "Romeo", :age => "15", :status => "alive"
         #your key/value pairs here
         },
       :hero_friends => []
