e15ce13c567f38ccb90fe4fc434de3b458083568
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-16 06:27:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,8 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriarch => {}, :matriarch => {}. :hero => {}, :hero_friends => []
+    :montague => {:patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []
       }, 
-    :capulet => {:
+    :capulet => {:patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []
       
       }
 
